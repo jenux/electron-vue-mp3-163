86f211710dfc58f459f438b31b8b3bb01420047d
Jenux <ijenux@gmail.com>
2019-09-17 21:18:36 +0800

    first commit: vue init simulatedgreg/electron-vue my-project

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-vue-mp3-163'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
